005b6320e80a3427c5da0f8c7529442325500483
yashimwong <yashimwong@gmail.com>
2022-01-27 07:28:17 +0800

    chore: add CSS Module typing configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,21 @@ import dts from 'vite-dts';
 
 const isExternal = (id: string) => !id.startsWith('.') && !path.isAbsolute(id);
 
+interface CSSModulesOptions {
+    scopeBehaviour?: 'global' | 'local';
+    globalModulePaths?: RegExp[];
+    generateScopedName?: string | ((name: string, filename: string, css: string) => string);
+    hashPrefix?: string;
+    /**
+     * default: null
+     */
+    localsConvention?: 'camelCase' | 'camelCaseOnly' | 'dashes' | 'dashesOnly' | null;
+}
+
 export default defineConfig(() => ({
+    css: {
+        postcss: {},
+    },
     esbuild: {
         jsxInject: "import React from 'react'",
     },
