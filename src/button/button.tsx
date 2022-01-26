4e32ab9ee236495faad8f2e93d5461d7531a2870
yashim-deriv <yashim@deriv.com>
2022-01-26 10:05:18 +0800

    chore: removed styled-components. bumped packages

--- src/button/button.tsx
+++ src/button/button.tsx
@@ -1,16 +1,7 @@
 import type { ButtonHTMLAttributes } from 'react';
-import styled from 'styled-components';
-
-const StyledButton = styled.button`
-    background-color: red;
-    border: none;
-    border-radius: 6px;
-    padding: 24px;
-    color: white;
-`;
 
 const Button = ({ children, ...props }: ButtonHTMLAttributes<HTMLButtonElement>) => {
-    return <StyledButton {...props}>{children}</StyledButton>;
+    return <button {...props}>{children}</button>;
 };
 
 export default Button;
