76f9b8602cbc380d1017eb86ea9272564afc23fb
yashim-deriv <yashim@deriv.com>
2022-02-07 15:20:01 +0800

    feat: add static assets to storybook deployment

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,6 @@ module.exports = {
     stories: ['../src/**/stories/**/*.stories.mdx', '../src/**/stories/**/*.stories.@(ts|tsx|mdx)'],
     addons: ['@storybook/addon-essentials', '@storybook/addon-docs', '@storybook/addon-links'],
     framework: '@storybook/react',
-    staticDirs: ['../static/**/*'],
     core: {
         builder: 'storybook-builder-vite',
     },
