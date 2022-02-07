2de357398da05c5400ba184bb09d1a255cfe2f9c
yashim-deriv <yashim@deriv.com>
2022-02-07 15:37:49 +0800

    feat: completed button variants

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,17 +1,18 @@
 import type { ButtonHTMLAttributes } from 'react';
 import classNames from 'classnames';
-import classes from './button.module.scss';
+import css from './button.module.scss';
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-    size?: 'small' | 'medium' | 'large';
-    color?: 'primary' | 'primary-light' | 'secondary';
+    size?: 'small' | 'medium' | 'large' | 'hero';
+    color?: 'primary' | 'primary-light' | 'secondary' | 'tertiary';
+    variant?: 'general' | 'hero';
     block?: boolean;
     dark?: boolean;
 }
 
-const Button = ({ children, block, color = 'primary', size = 'medium', ...props }: ButtonProps) => {
+const Button = ({ children, block, dark, color = 'primary', size = 'medium', ...props }: ButtonProps) => {
     return (
-        <button className={classNames(classes.btn, classes[size], classes[color], block && classes.full)} {...props}>
+        <button className={classNames(css.btn, css[size], css[color], block && css.full, dark && css.dark)} {...props}>
             {children}
         </button>
     );
