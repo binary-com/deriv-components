febbcc0da507de564ba2d31535ef4a259d768d1c
Bala <bala.krishna@regentmarkets.com>
2022-02-16 17:08:18 +0800

    configure webpack and remove vite

---
+++ webpack.config.ts
@@ -0,0 +1,44 @@
+import path from 'path';
+import webpack from 'webpack';
+
+const config: webpack.Configuration = {
+    mode: 'production',
+    entry: './src/index.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+                    },
+                },
+            },
+            {
+                test: /\.(s(a|c)ss)$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                            modules: true,
+                        },
+                    },
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'components.js',
+    },
+};
+
+export default config;
