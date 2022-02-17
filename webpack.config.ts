84a84bc76166015c9477e11745abd7471329afb8
Bala <bala.krishna@regentmarkets.com>
2022-02-17 14:32:30 +0800

    feat: multiple build setup

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,44 +1,64 @@
+import { ForkTsCheckerWebpackPlugin } from 'fork-ts-checker-webpack-plugin/lib/plugin';
 import path from 'path';
-import webpack from 'webpack';
+import { Configuration } from 'webpack';
 
-const config: webpack.Configuration = {
-    mode: 'production',
-    entry: './src/index.tsx',
-    module: {
-        rules: [
-            {
-                test: /\.(ts|js)x?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-                    },
-                },
-            },
-            {
-                test: /\.(s(a|c)ss)$/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
+type EnvConfig = {
+    modules: 'cjs' | 'es';
+};
+
+const config = (env: EnvConfig): Configuration => {
+    const modules = env.modules || 'cjs';
+    return {
+        mode: 'production',
+        devtool: 'source-map',
+        entry: './src/index.tsx',
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|js)x?$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
                         options: {
-                            importLoaders: 1,
-                            modules: true,
+                            presets: [
+                                [
+                                    '@babel/preset-env',
+                                    {
+                                        modules: modules === 'es' ? false : 'cjs',
+                                    },
+                                ],
+                                '@babel/preset-react',
+                                '@babel/preset-typescript',
+                            ],
                         },
                     },
-                    'sass-loader',
-                ],
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'components.js',
-    },
+                },
+                {
+                    test: /\.(s(a|c)ss)$/,
+                    use: [
+                        'style-loader',
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                importLoaders: 1,
+                                modules: true,
+                            },
+                        },
+                        'sass-loader',
+                    ],
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: `${modules}/components.js`,
+        },
+        externals: ['react'],
+        plugins: [new ForkTsCheckerWebpackPlugin()],
+    };
 };
 
 export default config;
