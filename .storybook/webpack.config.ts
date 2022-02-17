6e5295f3ce983c2d4fca8ecd6fce5a916b06db70
balakrishna-binary <56330681+balakrishna-binary@users.noreply.github.com>
2022-02-17 17:14:26 +0800

    bala/chore: fix assets path (#35)

--- .storybook/webpack.config.ts
+++ .storybook/webpack.config.ts
@@ -17,18 +17,5 @@ export default async ({ config }: { config: Configuration }) => {
         ],
     });
 
-    config.module?.rules?.push({
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        use: [
-            {
-                loader: 'file-loader',
-                options: {
-                    name: '[name].[ext]',
-                },
-            },
-        ],
-        include: path.resolve(__dirname, '../'),
-    });
-
     return config;
 };
