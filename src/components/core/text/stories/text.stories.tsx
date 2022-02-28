26f5953b92ae1e7fcfe1894650bad728b347d73a
Sean Ho <seanho96@yahoo.com>
2022-02-28 11:32:06 +0800

    chore: remove commented lines

--- src/components/core/text/stories/text.stories.tsx
+++ src/components/core/text/stories/text.stories.tsx
@@ -41,40 +41,6 @@ export default {
     },
 };
 
-// export const FullList = () => {
-//     return (
-//         <>
-//             <Text as="h1" type="hero">
-//                 Hero banner text
-//             </Text>
-//             <Text as="h1" type="heading-1">
-//                 Heading-1 text
-//             </Text>
-//             <Text as="h2" type="heading-2">
-//                 Heading-2 text
-//             </Text>
-//             <Text as="h3" type="heading-3">
-//                 Heading-3 text
-//             </Text>
-//             <Text as="h4" type="subtitle-1">
-//                 Subtitle-1 text
-//             </Text>
-//             <Text as="p" type="paragraph-1">
-//                 Paragraph-1 text
-//             </Text>
-//             <Text as="p" type="paragraph-2">
-//                 Paragraph-2 text
-//             </Text>
-//             <Text as="p" type="small">
-//                 Small text
-//             </Text>
-//             <Text as="p" type="extra-small">
-//                 Extra small text
-//             </Text>
-//         </>
-//     );
-// };
-
 export const Controls: Story<TextProps> = (args) => <Text {...args}>{args.children}</Text>;
 Controls.args = {
     children: 'Text',
