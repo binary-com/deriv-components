28dd80350e45ce46238dd4f9fd796dc45707bab0
Maryia <87321516+maryia-deriv@users.noreply.github.com>
2022-03-11 06:18:13 +0300

    Maryia/Added initial Wallet Card component & Stories & Tests (#53)
    * Maryia/Added initial wallet card component & story
    
    * Maryia/Tsconfig changes for successful build
    
    * Maryia/replaced require with import statement
    
    * Maryia/updated styles
    
    * Maryia/Improved stories
    
    * Maryia/Added background template and embed it using object
    
    * Maryia/improvements
    
    * Maryia/removed unnecessary change
    
    * Maryia/avoided removing object when it is already removed
    
    * Maryia/removed unnecessary dependency
    
    * Maryia/renamed a variable
    
    * Maryia/applied Text component
    
    * Maryia/walletcard setup for tests
    
    * Maryia/added unit tests & installed jest-dom
    
    * Maryia/replaced require with import in jest.setup file
    
    * Maryia/added active state & added secondary pattern color
    
    * Maryia/fixed styles for hover and active
    
    * Maryia/introduced wallets_data object to select color & logo based on wallet_name
    
    * Maryia/added a third color to Xpay
    
    * Maryia/refactoring
    
    * Maryia/added gradient to xpay background
    
    * Maryia/fixed the color of squares on the pattern
    
    * Maryia/update package-lock.json
    
    * Maryia/installed missing file-loader
    
    * Maryia/added the updated background (+ one for demo)
    
    * Maryia/updated wallets data
    
    * Maryia/improved switching from demo to other backgrounds
    
    * Maryia/added react-inlinesvg, more logos, small background & stories, disabled state, default_currency & display_name for each wallet, updated faded & active states
    
    * Maryia/relevant package-lock.json
    
    * Maryia/improved a condition for getting card text
    
    * Maryia/fix against wrong rendering in Storybook Docs related to elements having the same ids in each svg defs
    
    * Maryia/Updating elements' ids in logo svg to avoid wrong rendering in Docs

---
+++ src/components/wallet/card/wallet-card.tsx
@@ -0,0 +1,244 @@
+import classNames from 'classnames';
+import css from './wallet-card.module.scss';
+import DefaultBackground from '@assets/svg/card/default-background.svg';
+import DemoBackground from '@assets/svg/card/demo-background.svg';
+import SmallBackground from '@assets/svg/card/small-background.svg';
+import CheckIcon from '@assets/svg/card/ic-check.svg';
+import { useEffect, useState, useRef } from 'react';
+import Text from '@core/text/text';
+import SVG from 'react-inlinesvg';
+
+type TWalletCardData = {
+    [key: string]: {
+        colors: {
+            primary: string;
+            secondary: string;
+        };
+        default_currency: string;
+        display_name: string;
+    };
+};
+
+const wallet_card_data: TWalletCardData = {
+    aud: {
+        colors: { primary: '#0DB43D', secondary: '#FFCD00' },
+        default_currency: 'AUD',
+        display_name: 'AUD',
+    },
+    bitcoin: {
+        colors: { primary: '#F7931B', secondary: '#F7C71B' },
+        default_currency: 'BTC',
+        display_name: 'Bitcoin',
+    },
+    demo: {
+        colors: { primary: '#FF6444', secondary: '#FF444F' },
+        default_currency: 'USD',
+        display_name: 'Demo',
+    },
+    deriv_p2p: {
+        colors: { primary: '#FF444F', secondary: '#FF6444' },
+        default_currency: 'USD',
+        display_name: 'Deriv P2P',
+    },
+    ethereum: {
+        colors: { primary: '#52567F', secondary: '#828CAD' },
+        default_currency: 'ETH',
+        display_name: 'Ethereum',
+    },
+    eur: {
+        colors: { primary: '#283991', secondary: '#F8D12E' },
+        default_currency: 'EUR',
+        display_name: 'EUR',
+    },
+    gbp: {
+        colors: { primary: '#283991', secondary: '#F44336' },
+        default_currency: 'GBP',
+        display_name: 'GBP',
+    },
+    litecoin: {
+        colors: { primary: '#A5A8A9', secondary: '#C1CCCF' },
+        default_currency: 'LTC',
+        display_name: 'Litecoin',
+    },
+    payment_agent: {
+        colors: { primary: '#979797', secondary: '#B2C2C4' },
+        default_currency: 'USD',
+        display_name: 'Payment Agent',
+    },
+    tether: {
+        colors: { primary: '#009393', secondary: '#04D9D9' },
+        default_currency: 'USDT',
+        display_name: 'Tether',
+    },
+    usd: {
+        colors: { primary: '#F44336', secondary: '#283991' },
+        default_currency: 'USD',
+        display_name: 'USD',
+    },
+    usd_coin: {
+        colors: { primary: '#2775CA', secondary: '#224CE1' },
+        default_currency: 'USDC',
+        display_name: 'USD Coin',
+    },
+};
+
+export interface WalletCardProps {
+    active?: boolean;
+    balance?: string;
+    currency?: string;
+    dark?: boolean;
+    demo?: boolean;
+    disabled?: boolean;
+    faded?: boolean;
+    size?: 'small' | 'medium' | 'large';
+    wallet_name: string;
+}
+
+const WalletCard = ({
+    active,
+    balance,
+    currency,
+    dark,
+    demo,
+    disabled,
+    faded,
+    size = 'large',
+    wallet_name,
+}: WalletCardProps) => {
+    const [is_content_shown, setIsContentShown] = useState<boolean>(false);
+    const logo_src = `./modal/lg-${wallet_name?.replace('_', '-')}-${dark ? 'dark' : 'light'}.svg`;
+    const background =
+        size === 'small' ? SmallBackground : demo || wallet_name === 'demo' ? DemoBackground : DefaultBackground;
+    const fiat_wallets = ['aud', 'eur', 'gbp', 'usd'];
+    const wallet_title = wallet_card_data[wallet_name]?.display_name || wallet_name;
+    const wallet_currency = currency || wallet_card_data[wallet_name]?.default_currency;
+    const div_ref = useRef<HTMLDivElement>(null);
+
+    const updateBackground = () => {
+        // here we set background and pattern colors of svg background (svgs must be optimized with svgo):
+        const default_primary_color = dark ? '#303030' : '#CFCFCF';
+        const default_secondary_color = dark ? '#323738' : '#d6dadb';
+        const custom_primary_color = demo ? '#FF6444' : wallet_card_data[wallet_name]?.colors.primary;
+        const custom_secondary_color = demo ? '#FF444F' : wallet_card_data[wallet_name]?.colors.secondary;
+        const svg = div_ref.current?.querySelector('svg');
+        if (svg) {
+            svg.querySelectorAll('path')[0].setAttribute('fill', dark ? '#151717' : '#fff');
+            if (!demo && wallet_name !== 'demo' && size !== 'small') {
+                svg.querySelectorAll('path')[1]?.setAttribute('stroke', dark ? '#fff' : '#0E0E0E');
+            }
+            svg.querySelectorAll('circle').forEach((circle: SVGCircleElement, index: number) => {
+                // For large & medium size background: Primary color paints the 1st circle in the bottom-right corner & the 3rd circle in the top-left corner,
+                // and Secondary color paints the 2nd circle in the top-right corner,
+                // For small size background: Primary color paints the 1st circle on the left side of the background,
+                // and Secondary color paints the 2nd circle on the right side of the background:
+                if (index === 1) {
+                    circle.setAttribute('fill', custom_secondary_color || default_secondary_color);
+                } else circle.setAttribute('fill', custom_primary_color || default_primary_color);
+            });
+        }
+        // show card content when the background svg has loaded:
+        if (!is_content_shown) setIsContentShown(true);
+    };
+
+    useEffect(() => {
+        if (is_content_shown) updateBackground();
+    }, [dark, wallet_name, demo]);
+
+    const getCardText = () => {
+        if (balance) {
+            return (
+                <div>
+                    <Text as="div" type="extra-small" bold={false}>
+                        {demo ? 'Demo' : wallet_title} {fiat_wallets.every((f) => wallet_name !== f) && wallet_currency}{' '}
+                        wallet
+                    </Text>
+                    <Text as="div" type="paragraph-2" bold>
+                        {balance} {wallet_currency}
+                    </Text>
+                </div>
+            );
+        }
+        return (
+            <Text as="div" type="paragraph-2" bold={false}>
+                {demo ? 'Demo' : wallet_title} wallet
+            </Text>
+        );
+    };
+
+    return (
+        <div
+            data-testid="wallet-card"
+            className={classNames(
+                css.container,
+                css[size],
+                dark && css.dark,
+                disabled && css.disabled,
+                active && !disabled && is_content_shown && css.active,
+                faded && css.faded,
+            )}
+        >
+            <div ref={div_ref} className={css.background}>
+                <SVG
+                    src={background}
+                    viewBox={size === 'small' ? '0 0 64 40' : '0 0 240 144'}
+                    preserveAspectRatio="none"
+                    width="100%"
+                    height="100%"
+                    onLoad={updateBackground}
+                    preProcessor={(code) => {
+                        // A workaround to ensure that important ids in svg <defs> are unique to avoid issues with rendering in Storybook Docs:
+                        let updated_svg_string = code;
+                        const custom_id = size !== 'small' ? '1' : demo || wallet_name === 'demo' ? '2' : '3';
+                        [
+                            ['#a)', `#a${custom_id})`],
+                            ['"a"', `"a${custom_id}"`],
+                            ['#b)', `#b${custom_id})`],
+                            ['"b"', `"b${custom_id}"`],
+                            ['#c)', `#c${custom_id})`],
+                            ['"c"', `"c${custom_id}"`],
+                        ].forEach((el) => {
+                            updated_svg_string = updated_svg_string.replace(el[0], el[1]);
+                        });
+                        return updated_svg_string;
+                    }}
+                />
+            </div>
+            {active && !disabled && is_content_shown && (
+                <img className={css.active__icon} src={CheckIcon} alt={'active_icon'} />
+            )}
+            {is_content_shown && (
+                <div className={css.card_content}>
+                    {wallet_card_data[wallet_name] ? (
+                        <div
+                            className={classNames(css.logo, css[`logo__${wallet_name}`])}
+                            aria-label="payment_method_logo"
+                        >
+                            <SVG
+                                src={logo_src}
+                                width="100%"
+                                height="100%"
+                                preProcessor={(code) => {
+                                    let updated_svg_string = code;
+                                    const unique_id = Math.floor(Math.random() * 11) + 1;
+                                    // Adding a unique id to logo's linearGradient in svg <defs> to ensure correct rendering & setting viewBox if missing:
+                                    updated_svg_string = updated_svg_string.replaceAll(
+                                        /paint([0-9]+)_linear([_0-9]+)/g,
+                                        `plinear${unique_id}`,
+                                    );
+                                    return !updated_svg_string.includes('viewBox')
+                                        ? updated_svg_string.replace('svg', 'svg viewBox="0 0 64 40"')
+                                        : updated_svg_string;
+                                }}
+                            />
+                        </div>
+                    ) : (
+                        <div className={classNames(css.logo, css.logo__placeholder)}></div>
+                    )}
+                    {size !== 'small' && getCardText()}
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default WalletCard;
