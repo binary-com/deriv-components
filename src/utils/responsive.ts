e5135eab64619b711d7163aefbd064d764219966
Mohsen Hajibeigloo <mohsen@re-work.dev>
2022-12-02 15:44:54 +0330

    fix ssr issue window

--- src/utils/responsive.ts
+++ src/utils/responsive.ts
@@ -1,16 +1,4 @@
 export const MAX_MOBILE_WIDTH = 992;
 
-const isBrowser = typeof window !== 'undefined';
-
-export const isMobile = () => {
-    if (!isBrowser) {
-        return false;
-    }
-    return window.innerWidth <= MAX_MOBILE_WIDTH;
-};
-export const isDesktop = () => {
-    if (!isBrowser) {
-        return true;
-    }
-    window.innerWidth > MAX_MOBILE_WIDTH;
-};
+export const isMobile = () => window?.innerWidth <= MAX_MOBILE_WIDTH;
+export const isDesktop = () => window?.innerWidth > MAX_MOBILE_WIDTH;
