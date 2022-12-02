b6aef5853475ec9dd0695a89bc552ca760a70e62
Mohsen Hajibeigloo <mohsen@re-work.dev>
2022-12-02 16:14:59 +0330

    fix tests issue

--- src/utils/responsive.ts
+++ src/utils/responsive.ts
@@ -1,4 +1,16 @@
 export const MAX_MOBILE_WIDTH = 992;
 
-export const isMobile = () => window?.innerWidth <= MAX_MOBILE_WIDTH;
-export const isDesktop = () => window?.innerWidth > MAX_MOBILE_WIDTH;
+const isBrowser = typeof window !== 'undefined';
+
+export const isMobile = () => {
+    if (isBrowser) {
+        return window.innerWidth <= MAX_MOBILE_WIDTH;
+    }
+    return true;
+};
+export const isDesktop = () => {
+    if (isBrowser) {
+        return window.innerWidth > MAX_MOBILE_WIDTH;
+    }
+    return true;
+};
