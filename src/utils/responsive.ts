94c8519420877d9288676b416986dd52445a4a73
Mohsen Hajibeigloo <mohsen@re-work.dev>
2022-12-02 15:25:02 +0330

    fix: ssr issue with window object

--- src/utils/responsive.ts
+++ src/utils/responsive.ts
@@ -1,4 +1,16 @@
 export const MAX_MOBILE_WIDTH = 992;
 
-export const isMobile = () => window.innerWidth <= MAX_MOBILE_WIDTH;
-export const isDesktop = () => window.innerWidth > MAX_MOBILE_WIDTH;
+const isBrowser = typeof window !== 'undefined';
+
+export const isMobile = () => {
+    if (!isBrowser) {
+        return false;
+    }
+    return window.innerWidth <= MAX_MOBILE_WIDTH;
+};
+export const isDesktop = () => {
+    if (!isBrowser) {
+        return true;
+    }
+    window.innerWidth > MAX_MOBILE_WIDTH;
+};
